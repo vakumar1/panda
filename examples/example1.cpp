03058fd1f0ceb01195919a7a8401f29e1f418bab
Vasanth Kumar <vakumar@berkeley.edu>
2025-04-24 23:26:11 -0700

    [build] Add spec/tables dir script args

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -3,13 +3,40 @@
 #include "src/model/table.h"
 #include "src/panda.h"
 
-#include <unistd.h>
+#include <getopt.h>
 
-static std::string spec_dir("/home/vasanth/work/panda/examples/specs");
 static std::string spec_file("example1.yaml");
-static std::string table_dir("/home/vasanth/work/panda/examples/tables");
 
-int main(void) {
+int main(int argc, char* argv[]) {
+
+    const struct option long_options[] = {
+        {"spec_dir", required_argument, nullptr, 's'},
+        {"tables_dir", required_argument, nullptr, 't'},
+        {nullptr, 0, nullptr, 0}
+    };
+
+    int opt;
+    int long_index = 0;
+    std::string spec_dir;
+    std::string table_dir;
+    while ((opt = getopt_long(argc, argv, "s:t:", long_options, &long_index)) != -1) {
+        switch (opt) {
+            case 's':
+                spec_dir = optarg;
+                break;
+            case 't':
+                table_dir = optarg;
+                break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " [--spec_dir <spec directory>] [--tables_dir <tables directory>]" << std::endl;
+                return 1;
+        }
+    }
+    if (spec_dir.empty() || table_dir.empty()) {
+        std::string err_msg = "Provide spec dir and tables dir";
+        throw std::runtime_error(err_msg);
+    }
+
     Subproblem<int, double, std::string, int> init_subproblem = parse_spec<int, double, std::string, int>(spec_dir, spec_file, table_dir);
     std::unordered_map<Monotonicity<int, double, std::string, int>, Table<int, double, std::string, int>> output = generate_ddr_feasible_output<int, double, std::string, int>(init_subproblem);
     std::cout << "==== FEASIBLE DDR OUTPUT ====" << std::endl;
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,24 @@
 BUILD_DIR=~/.panda/build
 WORKSPACE_DIR=$(pwd)
-docker run \
-  -e USER="$(id -u)" \
-  -u="$(id -u)" \
-  -v $WORKSPACE_DIR:$WORKSPACE_DIR \
-  -v $BUILD_DIR:$BUILD_DIR \
-  -w $WORKSPACE_DIR \
-  gcr.io/bazel-public/bazel:latest \
-  --output_user_root=$BUILD_DIR \
-  run //examples:example1 --disk_cache=$BUILD_DIR --compilation_mode=dbg
+
+if [ $# -eq 0 ]; then
+  docker run \
+    -e USER="$(id -u)" \
+    -u="$(id -u)" \
+    -v $WORKSPACE_DIR:$WORKSPACE_DIR \
+    -v $BUILD_DIR:$BUILD_DIR \
+    -w $WORKSPACE_DIR \
+    gcr.io/bazel-public/bazel:latest \
+    --output_user_root=$BUILD_DIR \
+    run //examples:example1 --disk_cache=$BUILD_DIR --compilation_mode=dbg
+else
+  docker run \
+    -e USER="$(id -u)" \
+    -u="$(id -u)" \
+    -v $WORKSPACE_DIR:$WORKSPACE_DIR \
+    -v $BUILD_DIR:$BUILD_DIR \
+    -w $WORKSPACE_DIR \
+    gcr.io/bazel-public/bazel:latest \
+    --output_user_root=$BUILD_DIR \
+    run //examples:example1 --disk_cache=$BUILD_DIR --compilation_mode=dbg -- "$@"
+fi
